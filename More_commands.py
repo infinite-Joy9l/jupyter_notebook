38df19c17dfc85577cbdbca5b477cadf59c434d9
joydeep bhattacharjee <joydeep@nineleaps.com>
2017-11-22 20:18:08 +0530

    work o debugging and all

--- More_commands.py
+++ More_commands.py
@@ -14,7 +14,18 @@ file = FileLink('Magics.ipynb')
 file
 
 
+# # Debugging using pdb
+
 # In[*]
 
+import pdb
 
+def some_long_function():
+    x = 0
+    for i in range(10):
+        import pdb; pdb.set_trace()
+        x = i*2
+    return x
+
+print(some_long_function())
 
