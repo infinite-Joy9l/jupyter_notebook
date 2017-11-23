d8bfd55892a001dd45992957adea8e0016ab6522
joydeep bhattacharjee <joydeep@nineleaps.com>
2017-11-23 09:40:46 +0530

    bringing in line profiler ext

--- Magics.py
+++ Magics.py
@@ -14,12 +14,7 @@ df = pd.read_csv('reddituserpostingbehavior.csv.gz', compression='gzip', nrows=1
 
 # In[*]
 
-get_ipython().run_cell_magic('time', '', 'user_ids = []\nsubreddit_ids = []\nsubreddit_to_id = {}\ni = 0\n\nimport gzip\nwith gzip.open(\'reddituserpostingbehavior.csv.gz\', \'rb\') as f:\n    for line in f:\n        this_line = line.decode("utf-8")\n        for sr in this_line.rstrip().split(\',\')[1:]:\n            if sr not in subreddit_to_id:\n                subreddit_to_id[sr] = len(subreddit_to_id)\n            user_ids.append(i)\n            subreddit_ids.append(subreddit_to_id[sr])\n        i += 1')
-
-
-# In[*]
-
-get_ipython().run_cell_magic('time', '', "import numpy as np\nfrom scipy.sparse import csr_matrix\n\nrows = np.array(subreddit_ids)\ncols = np.array(user_ids)\ndata = np.ones((len(user_ids),))\nnum_rows = len(subreddit_to_id)\nnum_cols = i\n\n# the code above exists to feed this call.\nadj = csr_matrix((data, (rows, cols)), shape=(num_rows, num_cols))\nprint(adj.shape)\nprint('')")
+get_ipython().run_cell_magic('time', '', '\nimport gzip\n\ndef compute_user_subreddit_ids():\n    user_ids = []\n    subreddit_ids = []\n    subreddit_to_id = {}\n    i = 0\n    with gzip.open(\'reddituserpostingbehavior.csv.gz\', \'rb\') as f:\n        for line in f:\n            this_line = line.decode("utf-8")\n            for sr in this_line.rstrip().split(\',\')[1:]:\n                if sr not in subreddit_to_id:\n                    subreddit_to_id[sr] = len(subreddit_to_id)\n                user_ids.append(i)\n                subreddit_ids.append(subreddit_to_id[sr])\n            i += 1\n        return user_ids, subreddit_ids\n    \nuser_ids, subreddit_ids = compute_user_subreddit_ids()')
 
 
 # # Showing math equations
@@ -27,4 +22,40 @@ get_ipython().run_cell_magic('time', '', "import numpy as np\nfrom scipy.sparse 
 # In[*]
 
 get_ipython().run_cell_magic('latex', '', '\\begin{aligned}\n\\nabla \\times \\vec{\\mathbf{B}} -\\, \\frac1c\\, \\frac{\\partial\\vec{\\mathbf{E}}}{\\partial t} & = \\frac{4\\pi}{c}\\vec{\\mathbf{j}} \\\\\n\\nabla \\cdot \\vec{\\mathbf{E}} & = 4 \\pi \\rho \\\\\n\\nabla \\times \\vec{\\mathbf{E}}\\, +\\, \\frac1c\\, \\frac{\\partial\\vec{\\mathbf{B}}}{\\partial t} & = \\vec{\\mathbf{0}} \\\\\n\\nabla \\cdot \\vec{\\mathbf{B}} & = 0\n\\end{aligned}')
+
+
+# # showing profiling tools
+# 
+# The below command installs line profiler
+# 
+#     pip install line_profiler
+
+# In[*]
+
+get_ipython().magic('load_ext line_profiler')
+
+
+# In[*]
+
+# some small functions for example purposes
+import numpy as np
+
+def func(n, a):
+  y = np.arange(n)
+  y = np.exp(-y*a)
+  return y
+
+def gunc(n, a):
+  y = np.exp(-n*a)
+  return y
+
+def hunc(n, a):
+  y1 = func(n, a)
+  y2 = gunc(n, a)
+  return y1, y2
+
+
+# In[*]
+
+get_ipython().magic('lprun -f func hunc(50000, 0.5)')
 
